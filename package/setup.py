16a5be7df2723d757a3e201826025152c217efc3
Tim Magin <tim.magin@bahag.com>
2024-09-30 16:12:19 +0200

    new version

--- package/setup.py
+++ package/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.5'
+VERSION = '1.0.6'
 DESCRIPTION = 'Linter for Open API specification documents'
 LONG_DESCRIPTION = 'A yaml linter for opeanpi documents. '
 

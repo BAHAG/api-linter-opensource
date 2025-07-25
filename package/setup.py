791886481737a39fa33556136e64cb605e2548cb
Shubhushan Kattel <shubhushan.kattel@bahag.com>
2025-07-25 16:12:45 +0200

    Update: increase severity for rule b105 and update tests

--- package/setup.py
+++ package/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-VERSION = '1.0.8'
+VERSION = '1.0.9'
 DESCRIPTION = 'Linter for Open API specification documents'
-LONG_DESCRIPTION = 'A yaml linter for opeanpi documents. '
+LONG_DESCRIPTION = 'A yaml linter for openapi documents. '
 
 # Setting up
 setup(
